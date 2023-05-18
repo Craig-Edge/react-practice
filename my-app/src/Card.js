d13630de928427a07125040e8b9c172c0dac2b11
Craig Gordon <craig.gordon@edt.zone>
2023-05-18 11:31:27 +0100

    working example of class components

---
+++ my-app/src/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+/* In the className we are using the Tachyons css library, the following is what has been used:
+
+tc = Text Centre
+bg-light-green = background is light green
+dib = display inline block
+br3 = Border radius = 5rem
+pa3 = padding spacing medium
+ma2 = margin sapcing small
+grow = hover effect that makes the element grow
+bw2 = border width 25rem
+shadow-5 = box-shadow:4px 4px 8px 0px rgba( 0, 0, 0, 0.2 )
+
+*/
+
+// Empty angular brackets are shorthand for 'Fragment', this shorthand syntax does not support keys, React.Fragment does
+// Could have the destructuring of props passed in instead of passing in props and then destructuring
+const Card = (props) => {
+    const { name, email, id, nickname } = props;
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <> 
+                <h2>{name}</h2>
+                <h3>{nickname}</h3>
+                <p>{email}</p>
+            </>
+        </div>
+    )
+}
+
+export default Card;
---
+++ my-app/src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Conor Thomson',
+        username: 'Con Man',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Craig Gordon',
+        username: 'Boomer',
+        email: 'Insincere@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Lewis Ferrie',
+        username: 'Bob',
+        email: 'Honest@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Adrianna Ungureanu',
+        username: 'Newbie',
+        email: 'Earnest@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Boopalan Pilani',
+        username: 'Beep Boop',
+        email: 'StraightShooter@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Anubhav Singh',
+        username: 'Karaoke Master',
+        email: 'AManYouCanTrust@april.biz'
+    }
+]
