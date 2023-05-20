f567a18439721af7960a2ec83a9b0a7d1fad4ffa
Craig Gordon <craig.gordon@edt.zone>
2023-05-20 11:53:57 +0100

    created async action for requsting the data from the APi for the robofriends, added the required constants, combined the reducers into a root reducer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit'
+import { configureStore, applyMiddleware, combineReducers } from '@reduxjs/toolkit'
 import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
 const middleware = [logger];
-const store = configureStore({reducer: searchRobots, middleware});
+const store = configureStore({reducer: rootReducer, middleware});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
